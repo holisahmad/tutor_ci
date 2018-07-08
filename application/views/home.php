b2f5e40b4228f6cffa443e8f958fcba8b85bd6fb
Ahmad Holis <ahmadholis254@gmail.com>
2018-07-08 11:46:26 +0700

    perapihan

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,9 @@
         <!-- Page Content -->
         <div id="page-wrapper">
             <div class="container-fluid">
+                //lokasi isi
                 <?php $this->load->view($content); ?>
+
             </div>
         </div>
         <!-- /#page-wrapper -->
